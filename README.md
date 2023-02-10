a7fa74ef172ad14c3deb9b53d6b290c550d356ae
Kevin Greene <kpgreene131@gmail.com>
2023-02-10 13:40:38 -0500

    added some functions and readme

---
+++ README.md
@@ -0,0 +1,14 @@
+# ******************
+# CERITAS DATA LAYER
+# ******************
+
+ceritas_data_layer.py contains the class Ceritas_Database.
+This class starts up a connection to our Ceritas database, given correct credentials.
+There are a number of functions to help with querying the database.
+
+To create a database instance, initialize a class like this:
+```
+from ceritas_data_layer import Ceritas_Database
+
+db_instance = Ceritas_Database(database, user, password, host)
+```
--- ceritas_data_layer.py
+++ ceritas_data_layer.py
@@ -1,6 +1,6 @@
 import psycopg2
 
-class Database:
+class Ceritas_Database:
     def __init__(self, database, user, password, host):
         self._conn = psycopg2.connect(database=database,
                 user=user,
@@ -125,7 +125,7 @@ class Database:
 
     # core_product_id: id or list of id's to pull product info from
     # column: string or list of strings, specifying which columns to pull down. if blank, all columns are pulled down.
-    def get_product_info(self, core_product_id, column=None):
+    def get_product_info_by_id(self, core_product_id, column=None):
         if column is not None:
             column = self.list_to_sql(column)
         if type(core_product_id) is not list: core_product_id = [ core_product_id ]
@@ -133,6 +133,40 @@ class Database:
 
         self.cursor.execute("SELECT {column} FROM core_products WHERE id IN %s;".format(column=column or "*"), (product_tuple,))
         return self.fetchall()
+
+    # no inputs. grabs all core_product_ids which are linked to nvd_products
+    def get_vulnerable_products(self):
+        self.cursor.execute("SELECT core_product_id FROM nvd_products WHERE core_product_id IS NOT NULL;")
+        result = self.fetchall()
+        product_id_list = []
+        for item in result:
+            product_id_list.append(item[0])
+        return product_id_list
+
+    # product_id: product id or list of ids.
+    # column: string or list of strings, column names to pull from core_rating_history table. "value" is pulled by default
+    # returns rating value for each id passed. 
+    def get_product_rating(self, product_id, column=None):
+        if type(product_id) is not list: product_id = [ product_id ]
+        product_id_tuple = tuple()
+        for id in product_id:
+            product_id_tuple = product_id_tuple + (id,)
+
+        self.cursor.execute("SELECT current_rating_history_id FROM core_products WHERE id IN %s;", (product_id_tuple,))
+        result = self.fetchall()
+
+        core_rating_history_ids = tuple()
+        for item in result:
+            core_rating_history_ids = core_rating_history_ids + item
+
+        self.cursor.execute("SELECT {column} FROM core_rating_history WHERE id IN %s;".format(column=column or "value"), (core_rating_history_ids,))
+        result = self.fetchall()
+
+        rating_value_list = []
+        for item in result:
+            rating_value_list.append(item[0])
+
+        return rating_value_list
 
     # pass in product_id: get all vulnerabilities connected to this product
     # pass in list of product_id: get all vulnerabilities for list of products
@@ -153,36 +187,58 @@ class Database:
         for item in result:
             nvd_product_ids = nvd_product_ids + item
         
-        self.cursor.execute("SELECT id FROM nvd_cpe_matches WHERE nvd_product_id in %s", (nvd_product_ids,))
-        result = self.fetchall()
-        
-        nvd_cpe_match_ids = tuple()
-        for item in result:
-            nvd_cpe_match_ids = nvd_cpe_match_ids + item
+        if len(nvd_product_ids) > 0:
+            self.cursor.execute("SELECT id FROM nvd_cpe_matches WHERE nvd_product_id in %s", (nvd_product_ids,))
+            result = self.fetchall()
+            
+            nvd_cpe_match_ids = tuple()
+            for item in result:
+                nvd_cpe_match_ids = nvd_cpe_match_ids + item
 
-        self.cursor.execute("SELECT nvd_configuration_id FROM nvd_cpe_match_configuration WHERE nvd_cpe_match_id IN %s", (nvd_cpe_match_ids,))
-        result = self.fetchall()
-        
-        nvd_configuration_ids = tuple()
-        for item in result:
-            nvd_configuration_ids = nvd_configuration_ids + item
+            self.cursor.execute("SELECT nvd_configuration_id FROM nvd_cpe_match_configuration WHERE nvd_cpe_match_id IN %s", (nvd_cpe_match_ids,))
+            result = self.fetchall()
+            
+            nvd_configuration_ids = tuple()
+            for item in result:
+                nvd_configuration_ids = nvd_configuration_ids + item
 
-        self.cursor.execute("SELECT nvd_cve_id FROM nvd_configuration_cve WHERE nvd_configuration_id IN %s;", (nvd_configuration_ids,))
-        result = self.fetchall()
-        
-        nvd_cve_id_list = []
-        for item in result:
-            nvd_cve_id_list.append(item[0])
+            self.cursor.execute("SELECT nvd_cve_id FROM nvd_configuration_cve WHERE nvd_configuration_id IN %s;", (nvd_configuration_ids,))
+            result = self.fetchall()
+            
+            nvd_cve_id_list = []
+            for item in result:
+                nvd_cve_id_list.append(item[0])
 
-        return nvd_cve_id_list
+            return nvd_cve_id_list
+        else:
+            return -1
 
     # nvd_cve_id: an nvd_cve_id or list of ids.
     # column: column name or list of names. optional. specifies specific columns to pull rather than all
-    def get_cve_info(self, nvd_cve_id, column=None):
+    def get_cve_info_by_id(self, nvd_cve_id, column=None):
         if column is not None:
             column = self.list_to_sql(column)
         if type(nvd_cve_id) is not list: nvd_cve_id = [ nvd_cve_id ]
         nvd_cve_tuple = tuple(nvd_cve_id)
 
-        self.cursor.execute("SELECT {column} FROM nvd_cves WHERE id IN %s;".format(column=column or "*"), (nvd_cve_tuple,))
+        if len(nvd_cve_tuple) > 0:
+            self.cursor.execute("SELECT {column} FROM nvd_cves WHERE id IN %s;".format(column=column or "*"), (nvd_cve_tuple,))
+            return self.fetchall()
+        else:
+            return -1
+
+    # cve: cve string, for example: "CVE-2022-2222"
+    # column: column name or list of names. optional. pull specific columns, or all columns if blank
+    def get_cve_info_by_name(self, cve, column=None):
+        if column is not None:
+            column = self.list_to_sql(column)
+        if type(cve) is not list: cve = [ cve ]
+        cve = tuple(cve)
+
+        self.cursor.execute("SELECT {column} FROM nvd_cves WHERE cve IN %s;".format(column=column or "*"), (cve,))
         return self.fetchall()
+
+    def get_all_severities_for_product(self, product_id):
+        cve_ids = self.get_product_vulnerability_ids(product_id)
+        severities = self.get_cve_info_by_id(cve_ids, 'severity')
+        return severities
